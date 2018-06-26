a6b595b37e8231e5c588a980318ce5c5480cf592
usbpendrive <ferry.fernando@gmail.com>
2018-06-26 13:19:00 +0700

    Setup base react with webpack and babel.

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const webpack = require('webpack');
+const fs = require('fs');
+const path = require('path'),
+    join = path.join,
+    resolve = path.resolve;
+
+const getConfig = require('hjs-webpack');
+
+var config = getConfig({
+    in: join(__dirname, 'src/app.js'),
+    out: join(__dirname, 'dist'),
+    clearBeforeBuild: true
+});
+
+module.exports = config;
